74a1cb41fef2349f754a22641e8fec6e92c2dc2b
yuu <101458830+menyuu@users.noreply.github.com>
2022-05-18 04:14:00 +0000

    Bookモデルの作成booksコントローラーの作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  
+
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy ]
+
   devise_for :users
-  
+
   get 'home/about' => 'homes#about', as: 'about'
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
