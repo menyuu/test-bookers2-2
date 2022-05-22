00bbefcd15c0d8f974e83842650a27142505f27e
menyuu <101458830+menyuu@users.noreply.github.com>
2022-05-22 04:09:43 +0000

    searchの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,13 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
 
   has_one_attached :profile_image
-  
+
 has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
 has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
 has_many :followings, through: :relationships, source: :followed
 has_many :followers, through: :reverse_of_relationships, source: :follower
-  
+
 
 
   def get_profile_image(width, height)
@@ -26,18 +26,32 @@ has_many :followers, through: :reverse_of_relationships, source: :follower
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
-  
+
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
-  
+
   def unfollow(user_id)
     relationships.find_by(followed_id: user_id).destroy
   end
-  
+
   def following?(user)
     followings.include?(user)
   end
-  
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
 end
